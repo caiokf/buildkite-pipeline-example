9bb470191239f0c0a8117451997161cd9e94235c
Caio Kinzel Filho <caiokf@gmail.com>
2019-11-26 16:35:40 +0200

    using meta-data from previous steps

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,11 @@ if [ -z "$ENV" ]; then
 fi
 
 if [ -z "$F" ]; then
+  if [ "$ENV" == "prod" ]; then
+    TEAM=$(buildkite-agent meta-data get release-name)
+    STREAM=$(buildkite-agent meta-data get release-stream)
+    echo "Preparing to deploy $TEAM ($STREAM)"
+  fi
 	echo "Deploying to $ENV"
 	exit 0
 fi
